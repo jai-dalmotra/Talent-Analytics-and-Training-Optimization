2b277590971992e230e03539477703d26c5ff8db
Jai Dalmotra <168071500+jai-dalmotra@users.noreply.github.com>
2025-05-31 04:27:00 +0530

    Update test_sentiment.py

--- tests/test_sentiment.py
+++ tests/test_sentiment.py
@@ -3,15 +3,9 @@ from src.sentiment_analysis import add_sentiment_columns
 
 
 def test_sentiment_labels():
-    feedback_samples = [
-        "This was fantastic!",
-        "Not great, could be better.",
-        "Just okay."
-    ]
-
+    feedback_samples = ["This was fantastic!", "Not great, could be better.","Just okay."]
     sample_df = pd.DataFrame({'cleaned_feedback': feedback_samples})
     result_df = add_sentiment_columns(sample_df)
-
     assert 'tb_sentiment' in result_df.columns
     assert 'vader_sentiment' in result_df.columns
     assert set(result_df['tb_sentiment'].unique()) <= {"Positive", "Negative", "Neutral"}
